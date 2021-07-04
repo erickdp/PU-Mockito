b52e65e56b18c547993e6ee09084de0fe54c81e6
Erick Díaz <erickdp@hotmail.com>
2021-07-04 12:26:56 -0500

    Dependency Injection usando anotaciones de mock

--- src/test/java/org/ediaz/appmockito/services/ExamenServiceImpTest.java
+++ src/test/java/org/ediaz/appmockito/services/ExamenServiceImpTest.java
@@ -3,8 +3,12 @@ package org.ediaz.appmockito.services;
 import org.ediaz.appmockito.models.Examen;
 import org.ediaz.appmockito.repositories.ExamenRepository;
 import org.ediaz.appmockito.repositories.PreguntaRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,20 +17,37 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+//Para la inyeccion de dependencias con mock hay de 2 formas
+//1.- Para esto se debe de tener la dependencia de mock con junit jupiter, al usar esta no es necesario el beforeEach
+@ExtendWith(MockitoExtension.class)
 class ExamenServiceImpTest {
 
+    @Mock
     private ExamenRepository repository;
-    private ExamenServiceImp service;
+    @Mock
     private PreguntaRepository preguntaRepository;
+    @InjectMocks
+    private ExamenServiceImp service;
+
+    private TestInfo testInfo;
+    private TestReporter testReporter;
 
     //    Befor each para inicializar las instancias
     @BeforeEach
-    void beforeEach() {
+    void beforeEach(TestInfo testInfo, TestReporter testReporter) {
+        this.testInfo = testInfo;
+        this.testReporter = testReporter;
+
+        this.testReporter.publishEntry("Prueba Unitaria ejecutandoce -> "
+                .concat(testInfo.getTestMethod().orElseThrow().getName()));
+
+//        2.- Habilitando las anotaciones mock para esta instancia
+//        MockitoAnnotations.openMocks(this);
 //        Lo que hace Mockito es crear una clase que implementa la ainterfaz al vuelo
 //        Esto con el objetivo de evitar cambiar datos o agregar en una implementacion real
-        this.repository = mock(ExamenRepository.class);
-        this.preguntaRepository = mock(PreguntaRepository.class);
-        this.service = new ExamenServiceImp(repository, preguntaRepository);
+//        this.repository = mock(ExamenRepository.class);
+//        this.preguntaRepository = mock(PreguntaRepository.class);
+//        this.service = new ExamenServiceImp(repository, preguntaRepository);
     }
 
     @Test
@@ -53,11 +74,26 @@ class ExamenServiceImpTest {
     }
 
     @Test
+    @DisplayName("Encontrar examen y preguntas")
     void findExamenPorNombrePreguntas() {
         when(repository.findAll()).thenReturn(Datos.EXAMENES); // Se utilizan los datos de una clase de prueba
-        when(preguntaRepository.findPreguntasPorExamenId(1L)).thenReturn(Datos.PREGUNTAS); // Solo cuando haya el id 1 se traen los datos
-//        when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS); Se puede usar anyLong para obtener de cualquier id
+//        when(preguntaRepository.findPreguntasPorExamenId(1L)).thenReturn(Datos.PREGUNTAS); // Solo cuando haya el id 1 se traen los datos
+        when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS); // Se puede usar anyLong para obtener de cualquier id
         var examen = service.findExamenPorNombreConPreguntas("Analisis de datos");
         assertEquals(5, examen.getPreguntas().size());
     }
+
+    @Test
+    @Disabled
+    void findExamenPorNombrePreguntasVerify() {
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
+        when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+        var examen = service.findExamenPorNombreConPreguntas("Analisis de datos");
+        assertEquals(5, examen.getPreguntas().size());
+//        Para determinar si enrealidad se usa el metodo mediante mock se usa verify
+        verify(repository).findAll();
+//        En este caso fuerzo la falla porque se llamo el metodo para 1L pero valido que se haya llamado con 2L
+        verify(preguntaRepository).findPreguntasPorExamenId(2L);
+    }
+
 }
