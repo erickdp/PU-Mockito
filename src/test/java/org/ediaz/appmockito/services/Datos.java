86e010cd0f4aff2b32d4a4409748746e465d2dc0
Erick Díaz <erickdp@hotmail.com>
2021-07-04 11:49:24 -0500

    Dependencias Mock agregadas

---
+++ src/test/java/org/ediaz/appmockito/services/Datos.java
@@ -0,0 +1,18 @@
+package org.ediaz.appmockito.services;
+
+import org.ediaz.appmockito.models.Examen;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Datos {
+    public static final List<Examen> EXAMENES = Arrays.asList(
+            new Examen(1L, "Analisis de datos"),
+            new Examen(2L, "Seguridad de ti"),
+            new Examen(3L, "Marcos de desarrollo"));
+
+    public static final List<String> PREGUNTAS = Arrays.asList(
+            "aritmetica", "probabilidades",
+            "integrales", "derivadas", "ecuaciones"
+    );
+}
--- src/test/java/org/ediaz/appmockito/services/ExamenServiceImpTest.java
+++ src/test/java/org/ediaz/appmockito/services/ExamenServiceImpTest.java
@@ -25,18 +25,15 @@ class ExamenServiceImpTest {
 //        Lo que hace Mockito es crear una clase que implementa la ainterfaz al vuelo
 //        Esto con el objetivo de evitar cambiar datos o agregar en una implementacion real
         this.repository = mock(ExamenRepository.class);
+        this.preguntaRepository = mock(PreguntaRepository.class);
         this.service = new ExamenServiceImp(repository, preguntaRepository);
     }
 
     @Test
     void findExamenPorNombre() {
-        var datos = Arrays.asList(
-                new Examen(1L, "Analisis de datos"),
-                new Examen(2L, "Seguridad de ti"),
-                new Examen(3L, "Marcos de desarrollo"));
 //        Cada vez que se ejecute el metodo de findAll en la clase de service entonces retornara estos datos
 //        Se llama una simulacion del objeto ExamenRepository.class
-        when(repository.findAll()).thenReturn(datos);
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
 //        La simulacion solo se debe de hacer de metodos publicos no staticos final o private
 
         var examen = service.findExamenPorNombre("Analisis de datos");
@@ -48,11 +45,19 @@ class ExamenServiceImpTest {
 
     @Test
     void findExamenPorNombreListaVacia() {
-        List<Examen> datos = Collections.emptyList();
-        when(repository.findAll()).thenReturn(datos);
+        when(repository.findAll()).thenReturn(Collections.emptyList());
 
         var examen = service.findExamenPorNombre("Analisis de datos");
 
-        assertFalse(examen.isPresent()); //
+        assertFalse(examen.isPresent());
+    }
+
+    @Test
+    void findExamenPorNombrePreguntas() {
+        when(repository.findAll()).thenReturn(Datos.EXAMENES); // Se utilizan los datos de una clase de prueba
+        when(preguntaRepository.findPreguntasPorExamenId(1L)).thenReturn(Datos.PREGUNTAS); // Solo cuando haya el id 1 se traen los datos
+//        when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS); Se puede usar anyLong para obtener de cualquier id
+        var examen = service.findExamenPorNombreConPreguntas("Analisis de datos");
+        assertEquals(5, examen.getPreguntas().size());
     }
 }
